529c6d57cb09f4389017f416b4be7ef49fa56aac
hz <gowithwings@live.cn>
2017-06-26 11:20:59 +0200

    overall result function added

--- get_relate_info.py
+++ get_relate_info.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
 As filter to fetch content from database
 """
 import time
@@ -37,9 +35,9 @@ class trade_corp_info(object):
 
 class trade_overall_info(object):
     ### as transfer from source to target
-    def __init__(self, prov, name, time, volume, preis):
+    def __init__(self, prov, trade_type, time, volume, preis):
         self.prov = prov
-        self.name = name
+        self.type = trade_type
         self.time = time
         self.volume_deal = volume_deal
         self.preis_deal = preis_deal
@@ -51,7 +49,16 @@ def get_db_table_source():
     return ('`content`') 
 
 def get_db_table_trade_result_source():
-    return ('`corptrade`')     
+    return ('`corptrade`') 
+
+def get_db_table_trade_overall_source():
+    return ('`trade_Overall`')     
+
+def get_db_table_policy_source():
+    return ('`Timeline2`') 
+
+def get_db_company_source():
+    return ('`corp`')            
 
 def get_timestamp(string_input):
     st = time.strptime(string_input, '%Y-%m-%d')
@@ -125,6 +132,30 @@ def trade_set_sql_conditon(start = 0, end = 0, province = ''):
         return('null') 
     return(sql_query) 
 
+def policy_set_sql_conditon(start = 0, end = 0, province = '', key = ''):
+    ###
+    # @start: start time format %Y-%m-%d
+    # @end: end time format %Y-%m-%d
+    # @key: keyword specific for intro and title
+    # @province : specific for province
+    ###
+    # set time
+    time_delay = (datetime.datetime.now() - datetime.timedelta(days = 7))
+    t_start_default = time.strftime('%Y-%m-%d', time_delay.timetuple())
+    t_end_default = time.strftime('%Y-%m-%d', datetime.datetime.now().timetuple())
+    
+    end = t_end_default if (end == 0) else end
+    start = t_start_default if (start == 0) else start
+    
+    sql_query = " WHERE Date > '" + start + "' AND Date < '" + end + "' "
+    # set query province
+    if province != '':
+        sql_query += (" AND Prov LIKE '%" + province + "%' ")   
+    if key != '':
+        sql_query += (" AND Name LIKE '%" + key + "%' ")
+    print(sql_query)    
+    return(sql_query)    
+
 def fetch_content(sql_condition):
     ###
     # @ sql_condition shall be set by text_set_sql_condition function
@@ -165,14 +196,15 @@ def fetch_content(sql_condition):
     #print(res)    
     return res 
 
-def fetch_trade_company(sql_condition):
+def fetch_trade_company(sql_condition = '', company_id = 0):
     ###
     # @ sql_condition shall be set by text_set_sql_condition function
     ###
     res = []
-    if sql_condition == '' :
+    if sql_condition == '' and company_id == 0:
         return('failed')
-
+    if company_id != 0:
+        sql_condition = "WHERE ID = " + str(company_id) + "  ORDER BY T DESC"    
     connection_get = pymysql.connect(host = env.REMOTE_HOST,
                              port = env.REMOTE_PORT,
                              user = env.REMOTE_USER,
@@ -206,10 +238,166 @@ def fetch_trade_company(sql_condition):
                         new_trade_corp_info.preis
                     ])
 
+                     
     finally:
         connection_get.close()
     #print(res)    
     return res 
+
+def fetch_policy(sql_condition):
+    ###
+    # @ sql_condition shall be set by policy_set_sql_condition function
+    ###
+    res = []
+    if sql_condition == '' :
+        return('failed')
+
+    connection_get = pymysql.connect(host = env.REMOTE_HOST,
+                             port = env.REMOTE_PORT,
+                             user = env.REMOTE_USER,
+                             password = env.REMOTE_PW,
+                             db = env.REMOTE_DB,
+                             charset = 'gbk',
+                             cursorclass = pymysql.cursors.DictCursor)
+    try:
+        with connection_get.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT * FROM " + get_db_table_policy_source() + sql_condition
+            #print(sql)
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for item in result:
+                res.append([
+                    item['Name'],
+                    item['Date'],
+                    item['Link'],
+                    item['Prov']
+                ])
+
+    finally:
+        connection_get.close()
+    #print(res)    
+    return res 
+
+def fetch_company(company_name = '', company_id = 0):
+    res_info = [];
+    res = [];
+    if company_id == 0 and company_name == '':
+        return('Error. No info about the company is given')
+
+    connection_get = pymysql.connect(host = env.REMOTE_HOST,
+                             port = env.REMOTE_PORT,
+                             user = env.REMOTE_USER,
+                             password = env.REMOTE_PW,
+                             db = env.REMOTE_DB,
+                             charset = 'utf8',
+                             cursorclass = pymysql.cursors.DictCursor)
+
+    if company_id != 0:
+        sql = "SELECT * from " + get_db_company_source() + "WHERE ID=" + str(company_id)
+        try:
+            with connection_get.cursor() as cursor:
+                cursor.execute(sql)
+                result = cursor.fetchone()
+                res_info.append(str(
+                    " ID: " + str(result['ID']) + '\n' +
+                    "名称:" + result['N'] +
+                    "省：" + result['P'] +
+                    "市：" + result['City'] + '\n'
+                ))
+                res_info.append(str(
+                    "最高售电额：" + result['V_Y'] +
+                    "法人：" + result['Pres'] + '\n'
+                ))
+                res_info.append(str(
+                    "电话：" + result['Tel'] +
+                    "网站：" + result['Web'] +
+                    "邮箱：" + result['Email'] +
+                    "手机：" + result['Phone'] +
+                    "地址：" + result['Add'] + '\n'
+                ))
+                res_info.append(str("股东：" + result['Init'] +
+                    "经营范围：" + result['Addinfo']
+                ))
+                print(res_info)
+                res.append(res_info)    
+                trade_res = fetch_trade_company(company_id = company_id)    
+                if len(trade_res) > 0:
+                    res.append(["以上是公司基本信息\n"])
+                    res.append(["*********************************************\n"])
+                    res.append(["以下是公司交易信息\n"])
+                    for item in trade_res:    
+                        res.append(item)
+                trade_res = fetch_trade_overall(" WHERE Prov LIKE '%" + result['P'] + "%'")
+                if len(trade_res) > 0:
+                    res.append(["以上是公司交易信息\n"])
+                    res.append(["*********************************************\n"])
+                    res.append(["以下是公司所在省大盘信息\n"])
+                    for item in trade_res:    
+                        res.append(item)        
+        finally:
+            connection_get.close()
+    else: 
+        sql = "SELECT * from " + get_db_company_source() + "WHERE N LIKE '%" + str(company_name) + "%'"                                                
+        try:
+            with connection_get.cursor() as cursor:
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                print("总共有" + str(len(result)) + "个名字类似公司")
+                for item in result:
+                    print( "ID: " + str(item['ID']) + ":   " + item['N'])
+                print("之后设定 company_id 再搜索")    
+        finally:
+            connection_get.close()
+    return(res)
+
+def fetch_trade_overall(sql_condition):
+    ###
+    # @ sql_condition shall be set by policy_set_sql_condition function
+    ###
+    res = []
+    if sql_condition == '' :
+        return('failed')
+
+    connection_get = pymysql.connect(host = env.REMOTE_HOST,
+                             port = env.REMOTE_PORT,
+                             user = env.REMOTE_USER,
+                             password = env.REMOTE_PW,
+                             db = env.REMOTE_DB,
+                             charset = 'utf8',
+                             cursorclass = pymysql.cursors.DictCursor)
+    try:
+        with connection_get.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT * FROM " + get_db_table_trade_overall_source() + sql_condition
+            #print(sql)
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for item in result:
+                res.append([
+                    item['Prov'],
+                    item['Trade_type'],
+                    time.strftime("%Y %m", time.localtime(int(item['Time']))),
+                    item['P_deal'],
+                    item['V_deal']
+                ])
+
+    finally:
+        connection_get.close()
+    #print(res)    
+    return res     
+
+def trade_history(start = 0, end = 0, province = ''):
+    trade_sql_condition = trade_set_sql_conditon(start = start, province = province)
+    res = fetch_trade_company(trade_sql_condition)
+    trade_res = fetch_trade_overall(" WHERE Prov LIKE '%" + province + "%'")
+    if len(trade_res) > 0:
+        res.append(["以上是各公司历史交易数据\n"])
+        res.append(["*********************************************\n"])
+        res.append(["以下是该省大盘数据\n"])
+        for item in trade_res:    
+            res.append(item)
+    pd.DataFrame(res).to_csv('trade_result_company.csv', mode = 'w', header = False, index = False, encoding = 'gbk')
 
 def purify(str_input):
     ###
@@ -231,21 +419,33 @@ def main():
     # @start: start time format %Y-%m-%d
     # @end: end time format %Y-%m-%d
     # @key: keyword specific for intro and title
-    # @group : group range, default all sources, avaliable: 'main' or any single source,
-    #     mutil source unacceptable
     ###
+
     ###
     # 以下生成全文表格
-    text_sql_condition = text_set_sql_conditon(start = '2017-05-01', group = 'main', keys = ['山西', '售电'])
-    fulltxt = fetch_content(text_sql_condition)
-    pd.DataFrame(fulltxt).to_csv('content.csv', mode = 'w', header = False, index = False, encoding = 'utf-8')
+    # text_sql_condition = text_set_sql_conditon(start = '2017-05-01', group = 'main', keys = ['山西', '售电'])
+    # fulltxt = fetch_content(text_sql_condition)
+    # pd.DataFrame(fulltxt).to_csv('content.csv', mode = 'w', header = False, index = False, encoding = 'utf-8')
     # 以上生成全文表格 用#开关
-    # 以下生成历史交易数据
-    trade_sql_condition = trade_set_sql_conditon(start = '2017-5-10', province = '广东')
-    res = fetch_trade_company(trade_sql_condition)
-    pd.DataFrame(res).to_csv('trade_result_company.csv', mode = 'w', header = False, index = True, encoding = 'gbk')
-    # 以上生成历史交易 用#开关
 
+    # 以下生成历史交易数据
+    # trade_history(start = '2017-5-10', province = '广东')
+    # 以上生成历史交易 用#开关, 
+
+    # 以下生成历史政策数据
+    # policy_sql_condition = policy_set_sql_conditon(start = '2016-5-10', province = '江西')
+    # res = fetch_policy(policy_sql_condition)
+    # pd.DataFrame(res).to_csv('policy_result.csv', mode = 'w', header = False, index = True, encoding = 'gbk')
+    # 以上生成历史政策数据
+
+    # 以下生成公司有关数据
+    # 可以用名字模糊查询出ID
+    # res = fetch_company(company_name = '新奥')
+    # 利用ID查询结果
+    res = fetch_company(company_id = 265)
+    pd.DataFrame(res).to_csv('company_result.csv', mode = 'w', header = False, index = False, encoding = 'gbk')
+    # 以上生成公司有关数据
+    pass
 
 if __name__ == "__main__":
     main()
